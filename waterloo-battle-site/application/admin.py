6d192221c8d4810491fdb2e3458b8f7e58ee3d12
dan <dan__init__@outlook.com>
2026-09-11 00:29:05 +0100

    On branch dev  Changes to be committed: 	modified:   ../.gitignore 	modified:   application/admin.py 	new file:   application/migrations/0001_initial.py 	modified:   application/models.py 	new file:   application/static/application/main.js 	new file:   application/static/application/styles.css 	modified:   application/templates/application/battle_map.html

--- waterloo-battle-site/application/admin.py
+++ waterloo-battle-site/application/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+import application.models as models
 
-# Register your models here.
+admin.site.register(models.Personnel)
+admin.site.register(models.Army)
+admin.site.register(models.Corp)
+admin.site.register(models.Division)
+admin.site.register(models.Bridade)
+admin.site.register(models.Regiment)
+admin.site.register(models.Battalion)
+admin.site.register(models.Company)
--- waterloo-battle-site/application/models.py
+++ waterloo-battle-site/application/models.py
@@ -1,3 +1,109 @@
 from django.db import models
 
-# Create your models here.
+class Personnel(models.Model):
+    name = models.CharField(max_length=25) 
+    rank = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f"{self.name} {self.rank}"
+
+class Army(models.Model):
+    name = models.CharField(max_length=10)
+    primary_uniform_colour = models.CharField(max_length=10)
+    size = models.PositiveIntegerField()
+    commander = models.ForeignKey(
+        Personnel, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+class Corp(models.Model):
+    affiliated_army = models.ForeignKey(
+        Army, 
+        on_delete=models.SET,
+        null=True
+        )
+    name = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+    commander = models.ForeignKey(
+        Personnel, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+class Division(models.Model):
+    affiliated_corp = models.ForeignKey(
+        Corp, 
+        on_delete=models.SET_NULL,
+        null=True
+        )
+    name = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+    commander = models.ForeignKey(
+        Personnel, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+class Bridade(models.Model):
+    affilated_division = models.ForeignKey(
+        Division, 
+        on_delete=models.SET_NULL,
+        null=True
+        )
+    name = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+    commander = models.ForeignKey(
+        Personnel, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+class Regiment(models.Model):
+    affilated_brigade = models.ForeignKey(
+        Bridade, 
+        on_delete=models.SET_NULL,
+        null=True
+        )
+    name = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+    commander = models.ForeignKey(
+        Personnel, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+class Battalion(models.Model):
+    affliated_regiment = models.ForeignKey(
+        Regiment, 
+        on_delete=models.SET_NULL,
+        null=True
+        )
+    name = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+    commander = models.ForeignKey(
+        Personnel, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+class Company(models.Model):
+    affliated_battalion = models.ForeignKey(
+        Battalion, 
+        on_delete=models.SET_NULL,
+        null=True
+        )
+    name = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+    commander = models.ForeignKey(
+        Personnel, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
