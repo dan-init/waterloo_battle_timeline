0bc0421c0c8fc004e3c3821e090334376bcff961
dan <dan__init__@outlook.com>
2026-09-12 17:16:54 +0100

    On branch feat/#001-added-rank-model  Changes to be committed: 	modified:   .gitignore 	modified:   waterloo-battle-site/application/models.py
    added Rank model and foreign keyed it to Personnel model.

--- waterloo-battle-site/application/models.py
+++ waterloo-battle-site/application/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
+class Rank(models.model):
+    rank_title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.rank_title
+    
+
 class Personnel(models.Model):
     name = models.CharField(max_length=25) 
-    rank = models.CharField(max_length=25)
+    rank = models.ForeignKey(
+        Rank, 
+        on_delete=models.SET_NULL, 
+        null=True,
+        blank=True,
+        )
 
     def __str__(self):
         return f"{self.name} {self.rank}"
